668f2d3b426a67c75e5f0a0d3406bd128b6d8906
Zack Proser <zackproser@gmail.com>
2025-06-03 00:41:40 -0400

    Disable storage temporarily

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,10 +1,8 @@
 import { Mastra } from '@mastra/core';
-import { storage } from './memory';
 import { memeGeneratorAgent } from './agents/meme-generator';
 import { memeGenerationWorkflow } from './workflows/meme-generation';
 
 export const mastra = new Mastra({
-  storage,
   agents: {
     memeGenerator: memeGeneratorAgent,
   },
